8841aa09827b11af607015266e88d2db4776989a
Özgür Can <sezginzgurr@gmail.com>
2024-11-30 22:17:25 +0300

    first commit with love <3

---
+++ app/src/main/java/com/example/lovecounter/ui/MainActivity.kt
@@ -0,0 +1,35 @@
+package com.example.lovecounter.ui
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.navigation.compose.rememberNavController
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.ui.Modifier
+import com.example.lovecounter.ui.navigation.NavigationGraph
+import dagger.hilt.android.AndroidEntryPoint
+import com.example.lovecounter.ui.theme.MyappTheme
+
+@AndroidEntryPoint
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            MyappTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    val navController = rememberNavController()
+                    val startDestination = "Home"
+                    NavigationGraph(
+                        navController = navController,
+                        startDestination = startDestination,
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
