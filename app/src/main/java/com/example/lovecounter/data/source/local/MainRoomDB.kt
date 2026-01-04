a88086f220cb6c7e09d618dc12dff0644b9fe140
candroid <cnrture@gmail.com>
2026-01-05 01:36:43 +0300

    feat: remove MainEntityModel from Room database entities

--- app/src/main/java/com/example/lovecounter/data/source/local/MainRoomDB.kt
+++ app/src/main/java/com/example/lovecounter/data/source/local/MainRoomDB.kt
@@ -3,10 +3,9 @@ package com.example.lovecounter.data.source.local
 import androidx.room.Database
 import androidx.room.RoomDatabase
 import androidx.room.TypeConverters
-import com.example.lovecounter.data.model.MainEntityModel
 import com.example.lovecounter.data.model.Memory
 
-@Database(entities = [MainEntityModel::class, Memory::class], version = 1, exportSchema = false)
+@Database(entities = [Memory::class], version = 1, exportSchema = false)
 @TypeConverters(UriListConverter::class)
 abstract class MainRoomDB : RoomDatabase() {
     abstract fun mainDao(): MainDao
